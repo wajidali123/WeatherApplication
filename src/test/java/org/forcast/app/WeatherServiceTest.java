117ff2803a563a52e5aae8cea9100d53061af174
wajid_ali <wajid.iiui1@gmail.com>
2021-12-28 09:30:57 +0400

    junit test added for cityname and coordinates

--- src/test/java/org/forcast/app/WeatherServiceTest.java
+++ src/test/java/org/forcast/app/WeatherServiceTest.java
@@ -4,10 +4,13 @@
 package org.forcast.app;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.forcast.app.constant.MessagesCode;
 import org.forcast.app.dto.WeatherDto;
 import org.forcast.app.entity.UserEntity;
 import org.forcast.app.exception.BusinessException;
+import org.forcast.app.exception.ForcastException;
 import org.forcast.app.repository.UserRepository;
 import org.forcast.app.service.WeatherService;
 import org.junit.Test;
@@ -40,5 +43,33 @@ public class WeatherServiceTest {
 	public void checkLatAndLonIsNotNull() throws BusinessException, Exception {
 		WeatherDto weatherDto = weatherService.getWeatherDetailsByLatitudeAndLongitude(33.7104, 73.1338);
 		assertThat(weatherDto.getCoord()).isNotNull();
+	}
+	
+	@Test(expected = ForcastException.class)
+	public void emptycityandNameValidation() {
+		//Exception exception = (Exception) when(weatherService.getWeatherDetailsByCityName("", "")).thenThrow(new ForcastException(MessagesCode.EMPTY_CITYNAME));
+		try {
+			weatherService.getWeatherDetailsByCityName("", "");
+		} catch (BusinessException e) {
+			// TODO Auto-generated catch block
+			assertEquals(MessagesCode.EMPTY_CITYNAME, e.getErrorCode());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test(expected = ForcastException.class)
+	public void emptyLatAndLonValidation() {
+		//Exception exception = (Exception) when(weatherService.getWeatherDetailsByCityName("", "")).thenThrow(new ForcastException(MessagesCode.EMPTY_CITYNAME));
+		try {
+			weatherService.getWeatherDetailsByLatitudeAndLongitude(0.0d,0.0d );
+		} catch (BusinessException e) {
+			// TODO Auto-generated catch block
+			assertEquals(MessagesCode.INCORRECT_COORDINATES, e.getErrorCode());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
